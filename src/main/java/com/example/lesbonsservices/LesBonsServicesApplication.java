70a4c5bdb357c012493cbb71f802a53e0512f918
KrDevWeb <121158123+DayZmooN@users.noreply.github.com>
2026-01-22 14:40:49 +0100

    📁 Add backend package structure

--- src/main/java/com/example/lesbonsservices/LesBonsServicesApplication.java
+++ src/main/java/com/example/lesbonsservices/LesBonsServicesApplication.java
@@ -1,4 +1,4 @@
-package com.example.LesBonsServices;
+package com.example.lesbonsservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/example/lesbonsservices/LesBonsServicesApplicationTests.java
+++ src/test/java/com/example/lesbonsservices/LesBonsServicesApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.LesBonsServices;
+package com.example.lesbonsservices;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
