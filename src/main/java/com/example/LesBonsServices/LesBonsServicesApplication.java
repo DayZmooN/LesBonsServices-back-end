e32e3091d4d05ee351116d558cf7e39a7e0262c9
KrDevWeb <121158123+DayZmooN@users.noreply.github.com>
2026-01-22 13:54:34 +0100

    🚧 Set up project folder structure

---
+++ src/main/java/com/example/LesBonsServices/LesBonsServicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.LesBonsServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LesBonsServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LesBonsServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LesBonsServices/LesBonsServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LesBonsServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LesBonsServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
