f2854d4ceac8775e7e82d2a1120b88578961b3c9
KrDevWeb <121158123+DayZmooN@users.noreply.github.com>
2026-02-04 18:55:11 +0100

    test(controller): add MockMvc test for user registration endpoint (POST /api/auth/register) when return 400 for Invalid request payload

--- src/test/java/com/example/lesbonsservices/controller/RegistrationUserControllerTest.java
+++ src/test/java/com/example/lesbonsservices/controller/RegistrationUserControllerTest.java
@@ -1,16 +1,20 @@
 package com.example.lesbonsservices.controller;
 
+import com.example.lesbonsservices.dto.UserRegistrationRequestDto;
 import com.example.lesbonsservices.service.RegistrationUserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.server.ResponseStatusException;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -72,5 +76,29 @@ public class RegistrationUserControllerTest {
                 .andExpectAll(status().isCreated());
     }
 
+    /**
+     * Tests that the registration endpoint returns HTTP 400
+     * when provided with an invalid request payload.
+     */
+    @Test
+    void should_return_400_when_register_user_with_invalid_payload() throws Exception {
+        // Invalid registration request: phone is blank
+        String jsonRequest = """
+                {
+                    "email":"test@mail.com",
+                    "password":"toto123456",
+                    "firstName":"john",
+                    "lastName":"Doe",
+                    "phone":""
+                }
+                """;
 
+        // Perform HTTP POST request and verify response status
+        mockMvc.perform(post("/api/auth/register")
+                .contentType("application/json")
+                .content(jsonRequest))
+                .andExpect(status().isBadRequest());
+
+        verify(registrationUserService,never()).registrationUser(any());
+    }
 }
