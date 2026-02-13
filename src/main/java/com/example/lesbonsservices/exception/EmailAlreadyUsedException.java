9193d36956b50399b514e9051ad6885c389402bd
KrDevWeb <121158123+DayZmooN@users.noreply.github.com>
2026-02-13 17:44:32 +0100

    add: ResponseStatus HttpStatus CONFLICT for class EmailAlreadyUsedException

--- src/main/java/com/example/lesbonsservices/exception/EmailAlreadyUsedException.java
+++ src/main/java/com/example/lesbonsservices/exception/EmailAlreadyUsedException.java
@@ -1,6 +1,10 @@
 package com.example.lesbonsservices.exception;
 
-public class EmailAlreadyUsedException  extends RuntimeException{
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
+public class EmailAlreadyUsedException  extends RuntimeException {
     public EmailAlreadyUsedException(String message) {
         super(message);
     }
